37135ba8be3e05c9452481de822636fe48f3dffb
gabrielmapurunga <gabrielmapurunga@gmail.com>
2014-06-02 15:20:22 -0300

    commit da alteracao do menu

--- src/br/com/triadworks/sistema/cadastro/Menu.java
+++ src/br/com/triadworks/sistema/cadastro/Menu.java
@@ -7,8 +7,6 @@ public class Menu {
 		System.out.println("Entre com a opção desejada:");
 		System.out.println("'1' para se cadastrar.");
 		System.out.println("'2' para ver o cadastro.");
-		System.out.println("'3' para fechar o sistema.");
-		
 	}
 		
 }
